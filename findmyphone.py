e081fdc2e6dce1e68be7ec04ce5033e041256394
Chris Teater <cteater@hotmail.com>
2017-10-26 14:15:26 -0500

    Start
    Find with Bluetooth

---
+++ findmyphone.py
@@ -0,0 +1,12 @@
+import time
+from bt_proximity import BluetoothRSSI
+
+phone = '' #Bluetooth MAC address of phone
+
+def get_signal_stength(mac):
+    while(True):
+        time.sleep(0.2)
+        b = BluetoothRSSI(mac)
+        print(b.get_rssi())
+
+get_signal_stength(phone)
