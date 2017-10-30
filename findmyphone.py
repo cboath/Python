19a68c36a377812fcc564537c9b0d6b4e0ebf70f
Chris Teater <cteater@gmail.com>
2017-10-30 16:36:35 -0500

    Update findmyphone.py

--- findmyphone.py
+++ findmyphone.py
@@ -2,11 +2,17 @@ import time
 from bt_proximity import BluetoothRSSI #View readme for the prerequisites to run this.
 
 phone = '' #Bluetooth MAC address of phone
-
+dur = 6
+newavg = 0
 def get_signal_stength(mac):
-    while(True):
+    avg = 0
+    for i in range(0, dur):
         time.sleep(0.2)
         b = BluetoothRSSI(mac)
         print(b.get_rssi())
+        avg = avg + b.get_rssi()
+    global newavg
+    newavg = avg / dur
 
 get_signal_stength(phone)
+print('Average is ', newavg
