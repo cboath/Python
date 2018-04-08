a15f73333fd4398c6ac9a3b9b3898f613de0e3a8
Chris Teater <cteater@gmail.com>
2018-04-08 14:27:22 -0500

    Update StockMessin.py

--- StockMessin.py
+++ StockMessin.py
@@ -1,10 +1,14 @@
 from alpha_vantage.timeseries import TimeSeries
 ts = TimeSeries(key='', output_format='pandas')
 
-
-
-data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='compact')
+with open('list.txt', 'r') as stocks:
+    for x in stocks:
+        print('The stock is', x)
+        data, meta_data = ts.get_intraday(symbol=x.rstrip(), interval='1min', outputsize='compact')
  
-lastVal = data['4. close'].tail(1).to_string()
+        lastVal = data['4. close'].tail(1).to_string()
 
-print(lastVal)
+        data.describe()
+
+        singleVal = data['4. close'].tail(1).to_string()
+        print(singleVal)
