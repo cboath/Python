b8b427323e861d2a0fc9d66bc6011911001b62e4
Cboath <cteater@gmail.com>
2018-04-09 13:44:00 -0500

    More updates

--- StockMessin.py
+++ StockMessin.py
@@ -1,14 +1,16 @@
 from alpha_vantage.timeseries import TimeSeries
-ts = TimeSeries(key='', output_format='pandas')
+ts = TimeSeries(key='D3F0HVMFLMPCVLRN', output_format='pandas')
 
 with open('list.txt', 'r') as stocks:
     for x in stocks:
         print('The stock is', x)
         data, meta_data = ts.get_intraday(symbol=x.rstrip(), interval='1min', outputsize='compact')
- 
-        lastVal = data['4. close'].tail(1).to_string()
-
-        data.describe()
 
         singleVal = data['4. close'].tail(1).to_string()
-        print(singleVal)
+
+        a, b, c = singleVal.partition('    ')
+        g = float(c)
+
+        h= g*2
+
+        print(h)
--- alphaVantage.py
+++ alphaVantage.py
@@ -1,5 +1,5 @@
 from alpha_vantage.timeseries import TimeSeries
-ts = TimeSeries(key='', output_format='pandas')
+ts = TimeSeries(key='D3F0HVMFLMPCVLRN', output_format='pandas')
 data, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='compact')
 data.describe()
 print(data)
