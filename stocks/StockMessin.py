c9adda164f721f74d8f1809f61bb4148cda04dd9
Chris Teater <cteater@gmail.com>
2018-12-19 16:29:58 -0600

    Change order

--- stocks/StockMessin.py
+++ stocks/StockMessin.py
@@ -1,3 +1,12 @@
+#required_pkgs = ['alpha_vantage', 'pandas']
+#installed_pkgs = [pkg.key for pkg in pkg_resources.working_set]
+
+#for package in required_pkgs:
+    #if package not in installed_pkgs:
+        #print('Installing', package)
+        #subprocess.check_call(["python", '-m', 'pip', 'install', package])
+        
+        
 from alpha_vantage.timeseries import TimeSeries
 ts = TimeSeries(key='D3F0HVMFLMPCVLRN', output_format='pandas')
 
@@ -6,22 +15,15 @@ import pkg_resources
 import pip
 import subprocess
 
-required_pkgs = ['alpha_vantage', 'pandas']
-installed_pkgs = [pkg.key for pkg in pkg_resources.working_set]
-
-for package in required_pkgs:
-    if package not in installed_pkgs:
-        print('Installing', package)
-        subprocess.check_call(["python", '-m', 'pip', 'install', package])
-
 with open('list.txt', 'r') as stocks:
     for x in stocks:
-        print('The stock is', x.rstrip())
-        data, meta_data = ts.get_intraday(symbol=x.rstrip(), interval='1min', outputsize='compact')
+        currentSym = x.rstrip()
+        print('The stock is', currentSym)
+        data, meta_data = ts.get_intraday(symbol=currentSym, interval='1min', outputsize='compact')
 
         singleVal = data['4. close'].tail(1).to_string()
 
         a, b, c = singleVal.partition('    ')
         g = float(c)
 
-        print(g, '\n')
+        print(g)
