c73611c76915f2252d443fce681931544c904377
Chris Teater <cteater@gmail.com>
2019-04-17 16:04:41 -0500

    Uploads to twitter

--- cam/photobooth.py
+++ cam/photobooth.py
@@ -4,6 +4,8 @@ from time import sleep
 import RPi.GPIO as GPIO
 import pygame
 
+import tweettesting
+
 ##GPIO.setmode(GPIO.BCM)
 ##GPIO.setwarnings(False)
 ##GPIO.setup(17,GPIO.IN,GPIO.PUD_UP)
@@ -21,16 +23,18 @@ screen = pygame.display.set_mode(windowSize)
 myfont = pygame.font.Font(None, 600)
 labelPOS = (newWidth / 2, newHeight / 3)
 
+message = 'Tweeting another photo!'
+
 with picamera.PiCamera() as camera:
       camera.rotation = 180
-      camera.resolution = (1280, 768)
+      camera.resolution = (1280, 1024)
       camera.start_preview(fullscreen=False, window = (150, 110, 1680, 1050))
       camera.annotate_text_size = 45
       camera.annotate_background = Color('white')
       camera.annotate_foreground = Color('green')
       camera.annotate_text = 'Daugherty SA&E Summit 2019'
       screen.fill(black)
-      camera.preview.alpha = 128
+      camera.preview.alpha = 228
       #GPIO.wait_for_edge(17, GPIO.FALLING)
       shot = 0
       while shot < 2:
@@ -44,8 +48,10 @@ with picamera.PiCamera() as camera:
             #GPIO.output(GREEN_LED,False)
             sleep(1)
             pygame.display.flip()
-            camera.capture('img{counter:02d}.jpg')
+            photoName = 'img(%d).jpg' % shot
+            camera.capture(photoName)
             shot += 1
+            tweettesting.postTweets(photoName, message)
             screen.fill(black)
       camera.stop_preview()
       pygame.display.quit()
