04847a32050fb64939abf23ced172ec151c4ed3e
Chris Teater <cteater@gmail.com>
2020-05-10 20:41:56 -0500

    location update

--- cam/timelapse.py
+++ cam/timelapse.py
@@ -1,16 +1,27 @@
 from picamera import PiCamera
 from time import sleep
 import time
+import datetime
+import os
 
 camera = PiCamera()
 imgnum = 0
+imageloc = "/home/pi/Pictures/timelapse/"
+
+foldername = datetime.datetime.now()
+newname = foldername.strftime("%f")
+
+sourceloc = imageloc + newname
+
+os.makedirs(sourceloc)
 
 while True:
+    camera.rotation = 90
     file1 = "image{0:04d}.jpg".format(imgnum)
-    path = "/home/pi/Pictures/timelapse/" + file1
+    path = sourceloc + "/" + file1
     camera.capture(path)
     imgnum += 1
-    sleep(14400)
+    sleep(300)
     
 #Stitch code
 # avconv -r 10 -i image%04d.jpg -r 10 -vcodec libx264 -crf 20 -g 15 timelapse.mp4
@@ -20,3 +31,4 @@ while True:
 
 #Stitch application
 # sudo apt-get install libav-tools
+
