6544b33c852cf97a80b6112b47ab71997ca4621f
Chris Teater <cteater@gmail.com>
2020-10-12 00:03:05 -0500

    Delete jsonImport.py

--- jsonImport.py
+++
@@ -1,5 +0,0 @@
-import urllib.request, json
-
-with urllib.request.urlopen("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=demo")
-    data = json.loads(url.read().decode())
-    print(data)
